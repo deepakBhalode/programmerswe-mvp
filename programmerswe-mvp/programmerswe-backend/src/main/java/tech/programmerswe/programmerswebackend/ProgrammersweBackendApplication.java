f530b2dc2fb3d860f567a09518afc6added61e1c
deepakBhalode <deepak.bhalode.sbg@gmail.com>
2022-02-01 14:50:02 +0530

    initial structure for backend and frontend added

---
+++ programmerswe-mvp/programmerswe-backend/src/main/java/tech/programmerswe/programmerswebackend/ProgrammersweBackendApplication.java
@@ -0,0 +1,13 @@
+package tech.programmerswe.programmerswebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgrammersweBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgrammersweBackendApplication.class, args);
+	}
+
+}
---
+++ programmerswe-mvp/programmerswe-backend/src/test/java/tech/programmerswe/programmerswebackend/ProgrammersweBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.programmerswe.programmerswebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgrammersweBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
