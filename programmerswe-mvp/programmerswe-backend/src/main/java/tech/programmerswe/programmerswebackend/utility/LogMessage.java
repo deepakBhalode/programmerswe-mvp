b4d60bf52f27ae0971dd6d4ad01a1da98459aef5
deepakBhalode <deepak.bhalode.sbg@gmail.com>
2022-02-02 19:05:40 +0530

    SLF4J logging support configured

---
+++ programmerswe-mvp/programmerswe-backend/src/main/java/tech/programmerswe/programmerswebackend/utility/LogMessage.java
@@ -0,0 +1,16 @@
+package tech.programmerswe.programmerswebackend.utility;
+
+public class LogMessage {
+
+	private LogMessage() {
+		super();
+	}
+	
+	/* ======================== TRACE LEVEL LOGS ======================== */
+	public static final String IN_AUTHENTICATE_USER_API = "Inside authenticateUser API";
+	
+	
+	/* ======================== ERROR LEVEL LOGS ======================== */
+	
+	
+}
